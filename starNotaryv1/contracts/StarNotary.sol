d2dccf33a7c953914c6fab4380e1cb643953f629
tom <ssisksl77@gmail.com>
2019-06-27 22:50:41 +0900

    first notary project with DApp

---
+++ starNotaryv1/contracts/StarNotary.sol
@@ -0,0 +1,22 @@
+pragma solidity >=0.4.24;
+
+contract StarNotary {
+
+    string public starName;
+    address public starOwner;
+
+    event starClaimed(address owner);
+
+    constructor() public {
+        starName = "Awesome Udacity Star";
+    }
+
+    function claimStar() public {
+        starOwner = msg.sender;
+        emit starClaimed(msg.sender);
+    }
+
+    function changeName(string memory _name) public {
+      starName = _name;
+    }
+}
